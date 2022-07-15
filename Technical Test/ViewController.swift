6c0236934f56228029330336ed8424b372bdb379
Ghazi Tozri <g.tozri@digitsole.com>
2022-07-15 11:01:19 +0100

    Initial Commit

---
+++ Technical Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Technical Test
+//
+//  Created by Ghazi Tozri on 15/7/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
